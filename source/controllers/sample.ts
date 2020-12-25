4ae05e1cd18814e707077d82e1e4b14689745d2a
ahd <ahdhani0@gmail.com>
2020-12-25 16:24:43 +0300

    basic setup for nodejs express typescript

---
+++ source/controllers/sample.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from 'express';
+import logging from '../config/logging';
+
+const NAMESPACE = 'Sample Controller';
+
+const sampleHealthCheck = (req: Request, res: Response, next: NextFunction) => {
+    logging.info(NAMESPACE, `Sample Health check route called.`);
+    return res.status(200).json({ message: 'pong' });
+};
+
+export default { sampleHealthCheck };
